f09c378e8250efc9e78a66e01494bfcd812f5610
V-Play-Games <vaibhavnargwani28@gmail>
2022-04-10 21:33:52 +0530

    Added Capture#getShakeMessage with the pokemon capture math Added Capture.Method enum for how the mon was encountered.

--- src/main/java/net/vpg/bot/pokemon/Capture.java
+++ src/main/java/net/vpg/bot/pokemon/Capture.java
@@ -1,14 +1,110 @@
 package net.vpg.bot.pokemon;
 
-import net.vpg.bot.entities.PokemonSpecies;
+import net.vpg.bot.core.Range;
+import net.vpg.bot.entities.Item;
+import net.vpg.bot.entities.Pokemon;
 
 public class Capture {
+    private static final Range SHAKE_RANGE = Range.of(0, 255);
+
+    public static Message getShakeMessage(Pokemon pokemon, int selfLevel, int level, int maxHp, int currentHp, int turn, Item ball, StatusCondition status, Method method) {
+        if (ball.getId().equals("master-ball"))
+            return Message.SHAKE_4;
+        double hpModifier = 1 - (2.0 * currentHp) / (3 * maxHp);
+        int baseRate = pokemon.getSpecies().getCaptureRate();
+        double levelModifier = level > 20 ? 1 : (30 - level) / 10.0;
+        double rate = baseRate * hpModifier * levelModifier;
+        switch (status) {
+            case SLEEP:
+            case FREEZE:
+                rate *= 2.5;
+                break;
+            case PARALYSIS:
+            case POISON:
+            case TOXIC_POISON:
+            case BURN:
+                rate *= 1.5;
+                break;
+        }
+        switch (ball.getId()) {
+            case "master-ball": // shouldn't happen but putting here just in case
+            case "poke-ball":
+            case "premier-ball":
+            case "luxury-ball":
+                break;
+            case "great-ball":
+                rate *= 1.5;
+                break;
+            case "ultra-ball":
+                rate *= 2;
+                break;
+            case "level-ball":
+                if (selfLevel > 4 * level)
+                    rate *= 8;
+                else if (selfLevel > 2 * level)
+                    rate *= 4;
+                else if (selfLevel > level)
+                    rate *= 2;
+                break;
+            case "lure-ball":
+                if (method == Method.FISH)
+                    rate *= 4;
+                break;
+            case "heavy-ball":
+                // TODO: add weight to pokemon and implement this
+                break;
+            case "net-ball":
+                String type = pokemon.getType().getName();
+                if (type.contains("bug") || type.contains("water"))
+                    rate *= 3.5;
+                break;
+            case "nest-ball":
+                if (level < 30)
+                    rate *= (41 - level) / 10.0;
+                break;
+            case "repeat-ball":
+                // TODO: Figure out how to implement this (rate*3.5 if species is already caught)
+                break;
+            case "timer-ball":
+                rate *= Math.max(4, (1.0 + turn) * 1229 / 4096);
+                break;
+            case "dive-ball":
+                if (method == Method.SURF || method == Method.FISH || method == Method.DIVE)
+                    rate *= 3.5;
+                break;
+            case "dusk-ball":
+                if (method == Method.CAVE)
+                    rate *= 3.5;
+                break;
+            case "quick-ball":
+                if (turn == 1)
+                    rate *= 5;
+                break;
+            case "beast-ball":
+                if (pokemon.getAbilities()
+                    .stream()
+                    .map(Pokemon.AbilitySlot::getId)
+                    .anyMatch(id -> id.equals("beast-boost")))
+                    rate *= 5;
+                else
+                    rate *= 0.1;
+                break;
+            default:
+                throw new IllegalStateException(ball + " is not supported as a poke ball.");
+        }
+        // TODO: Figure out about critical captures
+        int shakeRange = (int) (65536 / Math.pow(255 / rate, 0.1875));
+        int i = 0;
+        while (i < 4 && SHAKE_RANGE.random() < shakeRange) i++;
+        return Message.values()[i];
+    }
+
     public enum Message {
-        SHAKE_0("", false),
-        SHAKE_1("", false),
-        SHAKE_2("", false),
-        SHAKE_3("", false),
-        SHAKE_4("", true);
+        SHAKE_0("Oh yikes! The Pokemon broke free!", false),
+        SHAKE_1("Darn it! It appeared to be caught!", false),
+        SHAKE_2("Argh! So close!", false),
+        SHAKE_3("Gah! Come on! You've gotta be kidding me!", false),
+        SHAKE_4("Woohoo! Gotcha! %s was caught!", true);
 
         private final String message;
         private final boolean caught;
@@ -25,5 +121,13 @@ public class Capture {
         public boolean isCaught() {
             return caught;
         }
+    }
+
+    public enum Method {
+        LAND,
+        CAVE,
+        SURF,
+        DIVE,
+        FISH;
     }
 }
